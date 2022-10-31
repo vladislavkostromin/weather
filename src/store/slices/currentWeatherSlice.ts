5430d3cda1ada0164c38117f7b09d9de2e066214
Vladislav Kostromin <61909342+vladislavkostromin@users.noreply.github.com>
2022-11-01 01:52:45 +0500

    store

---
+++ src/store/slices/currentWeatherSlice.ts
@@ -0,0 +1,56 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit"
+import { AxiosResponse } from 'axios'
+import { Weather } from "../types";
+
+type CurrentWeather = {
+  weather: Weather;
+  isLoading: boolean;
+  response: Response;
+};
+
+type Response = {
+  status: number;
+  message: string;
+};
+
+const initialState = {
+  weather: {},
+  isLoading: false,
+  response: {
+    status: 0,
+    message: ''
+  },
+};
+
+export const currentWeatherSlice = createSlice({
+  name: 'current_weather',
+  initialState,
+  reducers: {
+    fetchCurrentWeather(state) {
+      state.isLoading = true
+    },
+    fetchCurrentWeatherSuccess(
+      state, 
+      action: PayloadAction<AxiosResponse<Weather>>
+    ) {
+      state.weather = action.payload.data
+      state.isLoading = false
+      state.response = {
+        status: action.payload.status,
+        message: action.payload.statusText
+      }
+    },
+    fetchCurrentWeatherErr(
+      state, 
+      action: PayloadAction<AxiosResponse<Weather>>
+    ) {
+      state.isLoading = false
+      state.response = {
+        status: action.payload.status,
+        message: action.payload.statusText
+      }
+    },
+  }
+})
+
+export default currentWeatherSlice.reducer
